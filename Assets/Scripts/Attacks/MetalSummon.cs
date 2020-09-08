2f2ca5b57e696d4bc7b8dde7b48b1ffd2cb757ec
darthvader99999 <bhow245@gmail.com>
2020-09-07 23:14:57 -0700

    Moved old attack scripts to new directory. Set up skeleton class for metal magic

---
+++ Assets/Scripts/Attacks/MetalSummon.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MetalSummon : MonoBehaviour
+{
+    public int form;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void ChangeForm(int form){ //Switch to a different metal form (offence, defense, liquid)
+
+    }
+    public void ReturnToPlayer(){ //Lerp the metal back to the player
+
+    }
+    public void Attack(){
+        
+    }
+}
