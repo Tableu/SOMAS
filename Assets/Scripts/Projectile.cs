325454ed068995624a1bffc9aa6ec16eea2e6a68
darthvader99999 <bhow245@gmail.com>
2020-09-08 13:03:22 -0700

    Added Metal Sprites

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -6,6 +6,7 @@ public class Projectile : MonoBehaviour
 {
     public int damagePoints;
     public bool destructible = true;
+    public int healthPoints;
     // Start is called before the first frame update
     void Start()
     {
