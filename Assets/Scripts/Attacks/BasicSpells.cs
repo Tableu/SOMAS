303bffb537e26dae4b7720660a90321a846298f6
darthvader99999 <bhow245@gmail.com>
2020-11-17 00:46:51 -0800

    Reorganized spells into BasicSpells and FormSpells: - Deleted EarthMagic, FireMagic and WaterMagic. Replaced with BasicSpells and FormSpells which are attached directly to the player instead of child objects. - Replaced the element named inputactions with BasicSpell and FormSpell - Separated BasicSpells CastSpell into two parts: one for the start of a button press and one for the release. If a spell has been casted already the release will not trigger a spell.

---
+++ Assets/Scripts/Attacks/BasicSpells.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicSpells : MonoBehaviour
+{
+    public GameObject waterOrb;
+    public GameObject waterWave;
+    public GameObject flamethrower;
+    public GameObject earthWall;
+    private PlayerInput playerInput;
+    private PlayerInputActions playerInputActions;
+    private bool cast; //Prevents a water spell from casting if a spell has already been casted
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerInput = GetComponent<PlayerInput>();
+        playerInputActions = playerInput.playerInputActions;
+        cast = false;
+        playerInputActions.Player.BasicSpell.started += context => {
+            cast = false;
+            CastSpell();
+        };
+        playerInputActions.Player.BasicSpell.performed += context => {
+            WaterSpells(context.duration);
+        };
+    }
+    private void CastSpell(){
+        var attackDirection = playerInputActions.Player.HoldAttack.ReadValue<Vector2>();
+        if (attackDirection.Equals(Vector2.left) || attackDirection.Equals(Vector2.right)) {
+            Flamethrower(flamethrower);
+            cast = true;
+        }else if (attackDirection.Equals(Vector2.down)) {
+            EarthCreation(earthWall, 2,2);
+            cast = true;
+        }else if (attackDirection.Equals(Vector2.up)) {
+            
+        }
+    }
+    private void WaterSpells(double holdDuration){
+        if(cast)
+            return;
+        if (holdDuration > 1){
+            WaveAttack(waterWave, 10, new Vector2(2, 0));         
+        }else if (holdDuration > 0.5){
+            WaterOrb(waterOrb, 10, 10);
+        }
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void WaterOrb(GameObject projectilePrefab, float speed,float rotationSpeed){
+        var projectile = Instantiate(projectilePrefab, transform.position, Quaternion.identity);
+        projectile.GetComponent<Rigidbody2D>().velocity = transform.right*speed;
+        projectile.GetComponent<Rigidbody2D>().AddTorque(rotationSpeed);
+    }
+    private void WaveAttack(GameObject projectilePrefab, float speed, Vector2 displacement){
+        Vector3 playerForward = transform.right;
+        Vector3 dist = playerForward*displacement;
+        var angle = 0;
+        var projectile = Instantiate(projectilePrefab, transform.position+dist, Quaternion.identity);
+        projectile.GetComponent<Rigidbody2D>().velocity = playerForward*speed;
+        if(playerForward.x < 0){
+            angle = 180;
+        }
+        projectile.transform.rotation = Quaternion.Euler(0,angle,0);
+    }
+    private void Flamethrower(GameObject projectilePrefab){
+        Vector3 playerForward = transform.right;
+        var projectile = Instantiate(projectilePrefab, transform.position + new Vector3(7.5f*playerForward.x,0.2f,0), Quaternion.identity);
+        projectile.transform.parent = transform;
+        projectile.transform.rotation = Quaternion.Euler(0,0,80*playerForward.x);
+    }
+    private void EarthCreation(GameObject earthWallPrefab, float raycastLength, float position){
+        Vector3 playerForward = transform.right;
+        
+        var rayHit = Physics2D.Raycast(transform.position, Vector2.down, raycastLength, LayerMask.GetMask("Platforms"));
+        Debug.DrawRay(transform.position, Vector2.down*raycastLength,Color.red);
+
+        if (rayHit) {
+            Vector2 spawnPosition = transform.position + playerForward*position;
+            rayHit = Physics2D.Raycast(spawnPosition, Vector2.down, raycastLength, LayerMask.GetMask("Platforms"));
+            Debug.DrawRay(spawnPosition, Vector2.down*raycastLength,Color.red);
+            if (!rayHit)
+                return;
+            var wall = Instantiate(earthWallPrefab, spawnPosition, Quaternion.identity);
+            Destroy(wall, 2);
+        }else {
+            var platform = Instantiate(earthWallPrefab, transform.position+ new Vector3(0,-2,0), Quaternion.identity);
+            platform.transform.rotation = Quaternion.Euler(0,0,90);
+            Destroy(platform, 5);
+        }
+    }
+}
